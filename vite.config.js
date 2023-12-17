e690d6cd7eda4687b8646e587928380e7a13edc9
JFToro192 <juanfertoro192@gmail.com>
2023-12-17 17:38:47 +0100

    test route

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/xmas-git/',
+  base:'/babboNatale/',
   plugins: [vue()],
   build: {
     target: 'esnext'
